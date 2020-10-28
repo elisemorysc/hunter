9b445b61c6b30959eae77f3b42d9264b29367302
Eli Semory <elisemory@snap.com>
2020-10-28 16:05:54 -0400

    update to 1.9.5

--- cmake/projects/filament/hunter.cmake
+++ cmake/projects/filament/hunter.cmake
@@ -5,6 +5,7 @@
 
 include(hunter_add_version)
 include(hunter_cacheable)
+include(hunter_cmake_args)
 include(hunter_download)
 include(hunter_pick_scheme)
 
@@ -12,21 +13,18 @@ hunter_add_version(
     PACKAGE_NAME
     filament
     VERSION
-    1.9.2
+    1.9.5
     URL
-    "https://github.com/google/filament/archive/v1.9.2.zip"
+    "https://github.com/elisemorysc/filament/archive/hunter-v1.9.5.zip"
     SHA1
-    50beb3dbc0ee2ca2febb3466288a2754e5056b82
+    2e62b1fd7a2ec65804c9c86f71a3bcbae2718b12
 )
 
 hunter_cmake_args(
     filament
     CMAKE_ARGS
     FILAMENT_ENABLE_JAVA=OFF
-    FILAMENT_BUILD_FILAMAT=OFF
-    FILAMENT_SUPPORTS_METAL=OFF
     FILAMENT_SUPPORTS_VULKAN=OFF
-    ASSIMP_HUNTER_ENABLED=ON
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
