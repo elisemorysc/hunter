259c683fe8e48106c56f4a8928f12e84035c0159
Eli Semory <elisemory@snap.com>
2020-11-02 14:11:22 -0500

    test new version

--- examples/SPIRV-Tools/config.cmake
+++ examples/SPIRV-Tools/config.cmake
@@ -3,6 +3,6 @@ hunter_config(SPIRV-Headers
               SHA1 e7911f27a13c61e9d8c13fe7b0df5f1a883e972e)
 
 hunter_config(SPIRV-Tools 
-              URL https://github.com/elisemorysc/SPIRV-Tools/archive/v2020.1-new.tar.gz
-              SHA1 5402f9a7a6a1a5538ef2ee562be84176b10cad0a)
+              URL https://github.com/elisemorysc/SPIRV-Tools/archive/hunter-v2020.1-new.tar.gz
+              SHA1 0b8223f6285d16131d263882f93b99a8795036fe)
 
