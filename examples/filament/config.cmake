f9159380d6eb91517325244642b5891f3a90d968
Rahul Sheth <rahul@snap.com>
2020-11-09 11:12:02 -0500

    Update config.cmake

--- examples/filament/config.cmake
+++ examples/filament/config.cmake
@@ -10,5 +10,5 @@ hunter_config(spirv-cross VERSION 20200917)
 hunter_config(glslang VERSION 8.13.3743-9eef54b2-p0
               CMAKE_ARGS ENABLE_HLSL=OFF ENABLE_GLSLANG_BINARIES=OFF ENABLE_OPT=OFF BUILD_TESTING=OFF)
               
-hunter_config(astc-encoder VERSION 1.3-a47b80f-p0)
+hunter_config(astc-encoder VERSION 1.3-a47b80f-p1)
 
