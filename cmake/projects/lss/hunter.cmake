bec2e45ff221e421666252c6b6cda95f34b92e77
Eli Semory <esemory@snapchat.com>
2021-06-02 17:10:04 -0400

    Add inux-syscall-support

---
+++ cmake/projects/lss/hunter.cmake
@@ -0,0 +1,24 @@
+# Added by Denis Kerzhemanov, 2015
+
+# !!! DO NOT PLACE HEADER GUARDS HERE !!!
+
+include(hunter_add_version)
+include(hunter_download)
+include(hunter_pick_scheme)
+include(hunter_cacheable)
+include(hunter_configuration_types)
+
+hunter_add_version(
+    PACKAGE_NAME
+    lss
+    VERSION
+    "0.0.0-e1e7b0a-p0"
+    URL
+    "https://github.com/elisemorysc/linux-syscall-support/archive/refs/tags/e1e7b0a.tar.gz"
+    SHA1
+    3c2a69ddfa87628e0aa06d529755f29b14a07392
+)
+
+hunter_cacheable(lss)
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+hunter_download(PACKAGE_NAME lss)
